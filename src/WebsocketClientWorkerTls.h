db21c49e2b8dc0063a6d4bc8b36f5b63736e70cf
Manuel Schiller <manuel.schiller@bmw-carit.de>
2017-04-10 13:07:50 +0200

    added implementation of websocket client
    Change-Id: Ibe86d417678c969b3f8f33443374879602d596aa
    Reviewed-on: https://src.bmw-carit.de/gerrit/65660
    External-IP-Free: Manuel Schiller <manuel.schiller@bmw-carit.de>
    Reviewed-by: Donato Sciarra <donato.sciarra@bmw-carit.de>
    OSS-Check: Donato Sciarra <donato.sciarra@bmw-carit.de>
    Tested-by: zuul <noreply@bmw-carit.de>

---
+++ src/WebsocketClientWorkerTls.h
@@ -0,0 +1,76 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2017 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+#ifndef WSCPP_WEBSOCKETCLIENTWORKERTLS_H
+#define WSCPP_WEBSOCKETCLIENTWORKERTLS_H
+
+#include "GenericWebsocketClientWorker.h"
+
+namespace wscpp
+{
+
+class WebsocketClientWorkerTls : public GenericWebsocketClientWorker<websocketpp::config::asio_tls>
+{
+public:
+  WebsocketClientWorkerTls(std::unique_ptr<Parameters> parameters)
+      : GenericWebsocketClientWorker((std::move(parameters)))
+  {
+    using namespace std::placeholders;
+    endpoint.set_tls_init_handler(bind(&WebsocketClientWorkerTls::on_tls_init, this, _1));
+    start();
+  }
+
+  std::shared_ptr<boost::asio::ssl::context> on_tls_init(ConnectionHandle handle)
+  {
+    auto sslContext =
+        std::make_shared<boost::asio::ssl::context>(boost::asio::ssl::context::tlsv12_client);
+
+    if (parameters->tls.ca) {
+      for (const std::string& ca : parameters->tls.ca.get()) {
+        sslContext->add_certificate_authority(boost::asio::buffer(ca.data(), ca.size()));
+      }
+    }
+
+    if (!parameters->tls.cert->empty() && !parameters->tls.key->empty()) {
+      constexpr auto fileFormat = boost::asio::ssl::context_base::file_format::pem;
+      sslContext->use_private_key(
+          boost::asio::buffer(parameters->tls.key->data(), parameters->tls.key->size()),
+          fileFormat);
+      sslContext->use_certificate(
+          boost::asio::buffer(parameters->tls.cert->data(), parameters->tls.cert->size()),
+          fileFormat);
+    }
+    if ((parameters->tls.rejectUnauthorized && parameters->tls.rejectUnauthorized.get()) ||
+        !parameters->tls.rejectUnauthorized) {
+      sslContext->set_verify_mode(boost::asio::ssl::verify_peer |
+                                  boost::asio::ssl::verify_fail_if_no_peer_cert);
+    } else {
+      sslContext->set_verify_mode(boost::asio::ssl::verify_none);
+    }
+
+    if (parameters->tls.secureOptions) {
+      sslContext->set_options(parameters->tls.secureOptions.get());
+    }
+
+    return sslContext;
+  }
+};
+} // namespace wscpp
+
+#endif // WSCPP_WEBSOCKETCLIENTWORKERTLS_H
---
+++ src/wscpp-client.cpp
@@ -0,0 +1,124 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2017 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+#include <memory>
+
+#include <nan.h>
+
+#include "IWebsocketClientWorker.h"
+#include "WebsocketClientWorker.h"
+#include "WebsocketClientWorkerTls.h"
+
+namespace wscpp
+{
+class WebsocketClientWrapper : public Nan::ObjectWrap
+{
+public:
+  static NAN_MODULE_INIT(Init)
+  {
+    v8::Local<v8::FunctionTemplate> tpl = Nan::New<v8::FunctionTemplate>(New);
+    tpl->SetClassName(Nan::New("WebsocketClientWorker").ToLocalChecked());
+    tpl->InstanceTemplate()->SetInternalFieldCount(2);
+
+    SetPrototypeMethod(tpl, "send", send);
+    SetPrototypeMethod(tpl, "close", close);
+
+    constructor().Reset(Nan::GetFunction(tpl).ToLocalChecked());
+    Nan::Set(target, Nan::New("WebsocketClientWorker").ToLocalChecked(),
+             Nan::GetFunction(tpl).ToLocalChecked());
+  }
+
+private:
+  WebsocketClientWrapper(IWebsocketClientWorker* worker) : worker(worker) {}
+
+  ~WebsocketClientWrapper() override = default;
+
+  static NAN_METHOD(New)
+  {
+    if (info.Length() != 3) {
+      return Nan::ThrowError("Invalid number of arguments");
+    }
+
+    if (info.IsConstructCall()) {
+      try {
+        auto parameters = std::make_unique<Parameters>(info);
+        IWebsocketClientWorker* worker = nullptr;
+        if (parameters->serverUri->get_secure()) {
+          worker = new WebsocketClientWorkerTls(std::move(parameters));
+        } else {
+          worker = new WebsocketClientWorker(std::move(parameters));
+        }
+
+        WebsocketClientWrapper* wrapper = new WebsocketClientWrapper(worker);
+
+        wrapper->Wrap(info.This());
+        info.GetReturnValue().Set(info.This());
+
+      } catch (const std::exception& e) {
+        Nan::ThrowError(e.what());
+        info.GetReturnValue().SetUndefined();
+        return;
+      }
+
+    } else {
+      const int argc = 3;
+      v8::Local<v8::Value> argv[argc] = {info[0], info[1], info[2]};
+      v8::Local<v8::Function> cons = Nan::New(constructor());
+      info.GetReturnValue().Set(Nan::NewInstance(cons, argc, argv).ToLocalChecked());
+    }
+  }
+
+  static NAN_METHOD(send)
+  {
+    Nan::HandleScope scope;
+    if (info.Length() != 2) {
+      return Nan::ThrowError("Invalid number of arguments");
+    }
+    const bool isBinary = info[1]->BooleanValue();
+    v8::Local<v8::Object> buffer = info[0]->ToObject();
+    const char* data = node::Buffer::Data(buffer);
+    const std::size_t size = node::Buffer::Length(buffer);
+
+    auto wrapper = Nan::ObjectWrap::Unwrap<WebsocketClientWrapper>(info.Holder());
+
+    if (isBinary) {
+      wrapper->worker->sendBinaryMessage(data, size);
+    } else {
+      wrapper->worker->sendTextMessage(data, size);
+    }
+  }
+
+  static NAN_METHOD(close)
+  {
+    auto wrapper = Nan::ObjectWrap::Unwrap<WebsocketClientWrapper>(info.Holder());
+    wrapper->worker->close();
+  }
+
+  static inline Nan::Persistent<v8::Function>& constructor()
+  {
+    static Nan::Persistent<v8::Function> constructorFunctino;
+    return constructorFunctino;
+  }
+
+  IWebsocketClientWorker* worker;
+};
+
+} // namespace wscpp
+
+NODE_MODULE(websocketpp_client, wscpp::WebsocketClientWrapper::Init)
